e80591977b8eedcac9aed45979f6029d8a98b2ba
SantiQ <a01571222@itesm.mx>
2025-02-13 21:41:09 -0600

    Chambers done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/Chambers/LowChamber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/Chambers/LowChamber.java
@@ -6,6 +6,7 @@ import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import org.firstinspires.ftc.teamcode.Commands.Arm.MoveArm;
 import org.firstinspires.ftc.teamcode.Commands.Elevator.ElevatorPositions;
+import org.firstinspires.ftc.teamcode.Commands.Intake.MoveIntake;
 import org.firstinspires.ftc.teamcode.Commands.StowAll;
 import org.firstinspires.ftc.teamcode.Commands.WaitForButton;
 import org.firstinspires.ftc.teamcode.Commands.Wrist.MoveWrist;
@@ -13,19 +14,19 @@ import org.firstinspires.ftc.teamcode.Subsystems.Arm;
 import org.firstinspires.ftc.teamcode.Subsystems.Constants;
 import org.firstinspires.ftc.teamcode.Subsystems.Elevator;
 import org.firstinspires.ftc.teamcode.Subsystems.Wrist;
-
+import org.firstinspires.ftc.teamcode.Subsystems.Intake;
 
 
 public class LowChamber extends SequentialCommandGroup {
 
-    public LowChamber (Arm arm, Elevator elevator, Wrist wrist, GamepadEx operatorGamepad){
+    public LowChamber (Arm arm, Elevator elevator, Wrist wrist, GamepadEx operatorGamepad, Intake intake){
         addCommands(
+                new MoveIntake(intake, Constants.Intake.INTAKE_STOW),
                 new MoveArm(arm, Constants.Arm.ARM_LOWCHAMBER).withTimeout(500),
-                new ElevatorPositions(elevator, Constants.Elevator.ELEVATOR_LOWCHAMBER),
+                new MoveWrist(wrist, 0.3),
                 new WaitForButton(operatorGamepad, GamepadKeys.Button.X),
                 new MoveWrist(wrist, 0.4),
-                new WaitForButton(operatorGamepad, GamepadKeys.Button.X),
-                new MoveArm(arm,  14.0 ).withTimeout(500)
+                new MoveArm(arm, 0.6).withTimeout(500)
         );
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/GroundGrab/GrabSpecimensOrientation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/GroundGrab/GrabSpecimensOrientation.java
@@ -15,15 +15,22 @@ import org.firstinspires.ftc.teamcode.Commands.Elevator.ElevatorPositions;
 import org.firstinspires.ftc.teamcode.Subsystems.Wrist;
 import org.firstinspires.ftc.teamcode.Commands.WaitForButton;
 import org.firstinspires.ftc.teamcode.Subsystems.Intake;
+import org.firstinspires.ftc.teamcode.Subsystems.Elevator;
 
-    public class GrabSpecimens extends SequentialCommandGroup {
-    public GrabSpecimens (Arm arm, GamepadEx operatorGamepad, Wrist wrist, Intake intake, Elevator elevator){
+public class GrabSpecimensOrientation extends SequentialCommandGroup {
+    public GrabSpecimensOrientation (Arm arm, GamepadEx operatorGamepad, Wrist wrist, Intake intake, Elevator elevator){
         addCommands(
+                new MoveArm(arm,5.0).withTimeout(100),
+                new MoveWrist(wrist, 0.5),
+                new MoveIntake(intake, Constants.Intake.INTAKE_OPEN),
+                new WaitForButton(operatorGamepad, GamepadKeys.Button.START),
                 new MoveIntake(intake, Constants.Intake.INTAKE_STOW),
-                new MoveWrist(wrist, 0.05),
-                new WaitForButton(operatorGamepad, GamepadKeys.Button.LEFT_BUMPER),
-                new MoveArm(arm, 0),
+                new MoveArm(arm, 15.0).withTimeout(200),
+                new MoveWrist(wrist, Constants.Wrist.WRIST_STOW),
                 new StowAll(arm, elevator, wrist)
         );
     }
 }
+
+
+
